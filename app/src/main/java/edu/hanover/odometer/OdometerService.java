01a0d2cb87078644f452436d71f675b71f0d6c87
Hannah Mundt <mundth19@hanover.edu>
2018-05-20 15:15:34 -0400

    added location listener

--- app/src/main/java/edu/hanover/odometer/OdometerService.java
+++ app/src/main/java/edu/hanover/odometer/OdometerService.java
@@ -1,12 +1,19 @@
 package edu.hanover.odometer;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
 
 
 public class OdometerService extends Service {
+    private static double distanceInMeters;
+    private static Location lastLocation = null;
     private final IBinder binder = new OdometerBinder();
     public OdometerService() {
     }
@@ -21,4 +28,25 @@ public class OdometerService extends Service {
             return OdometerService.this;
         }
     }
+
+    @Override
+    public void onCreate() {
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+            @Override
+            public void onProviderDisabled(String arg0) {}
+            @Override
+            public void onProviderEnabled(String arg0) {}
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle bundle) {}
+        };
+    }
+
 }
